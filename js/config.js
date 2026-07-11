1bd5cf3bef3228d271d0e241209c181355d3f9cf
Mohsen Reyhani <37334026+MohsenReyhani@users.noreply.github.com>
2026-07-11 19:35:44 +0330

    Update WEB_APP_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 
 // config.js
 const CONFIG = {
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwA2NhRInp4Jfl5TYVyC7adIo7WqYXc7iLMibc770As_y8d2kS9qXMoXKJjAPrH2TG6/exec",
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyArz6L0FDeRjgbn_z0jk5WrlNiQ8m3Tgz2OLo0Jb2SbjH1WA8ULCzNL6EohMHqM_OSxg/exec",
     VERSION: "1.3"
 };
