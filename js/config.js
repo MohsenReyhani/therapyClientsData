0bed7c09826f72d050e4c41c7e45c0c0c0e68100
MohsenReyhani <mohsenreyhani@gmail.com>
2026-07-27 19:31:55 +0330

    version update

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 // config.js
 const CONFIG = {
     WEB_APP_URL: "https://script.google.com/macros/s/AKfycbx5i1Pz7kZocYNncHv8r1pJZsKpz6FIqCEyRvJyRnNubrkKsF0bpTmF8Ihp2ctmX0z2/exec",
-    VERSION: "1.5"
+    VERSION: "1.6"
 };
